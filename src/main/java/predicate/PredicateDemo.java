1b4480b1192770a33dec64e116753728da399e46
Jaideep Kumar Rana <jaideepk@thoughtworks.com>
2020-03-30 18:27:30 +0530

    JD | Add example of predicate

--- src/main/java/predicate/PredicateDemo.java
+++ src/main/java/predicate/PredicateDemo.java
@@ -16,5 +16,11 @@ public class PredicateDemo {
 
         evenNumbers.stream().forEach(number -> System.out.println("Even no: " + number));
         numbersDivisibleByThree.stream().forEach(number -> System.out.println("No divisible by 3: " + number));
+
+
+        System.out.println("Is 4 divisible by 2? " + Math.isEven.test(4));
+        System.out.println("3 'not' divisible by 2? " + Math.isEven.negate().test(3));
+        System.out.println("Is the number 27 either divisible 3 or 2? " + Math.isEven.or(Math.isDivisibleByThree).test(27));
+        System.out.println("Is the number 7 either divisible 3 or 2? " + Math.isEven.or(Math.isDivisibleByThree).test(7));
     }
 }
